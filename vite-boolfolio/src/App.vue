9280504d3936f6c6383bd7c11948131730525fc1
VitoMarino <marino.vito2001@gmail.com>
2024-08-21 17:13:23 +0200

    Inizio Milestone 5

--- vite-boolfolio/src/App.vue
+++ vite-boolfolio/src/App.vue
@@ -2,13 +2,15 @@
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
+import ProjectCard from './components/ProjectCard.vue';
 
 export default {
   name: '',
   components: {
     AppHeader,
     AppMain,
-    AppFooter
+    AppFooter,
+    ProjectCard
   }
 }
 </script>
@@ -16,6 +18,7 @@ export default {
 <template>
 <AppHeader/>
 <AppMain/>
+<ProjectCard/>
 <AppFooter/>
 </template>
 
